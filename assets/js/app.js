a071ba0b768cac1561680e70b9c3a68631640a01
lalitverma01 <lalitverma2923@gmail.comm>
2025-02-17 10:38:00 +0530

    PSA-Page

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate42")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates2").classList.toggle("rotate43")
+    document.body.classList.toggle("overflow-hidden")
+}
