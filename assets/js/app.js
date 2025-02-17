98ff3632c407f6b6871fb63b82150168609c9a00
lalitverma01 <lalitverma2923@gmail.comm>
2025-02-17 11:33:21 +0530

    slider-done

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,36 @@ const navbarbtn = () => {
     document.getElementById("rotates2").classList.toggle("rotate43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+$('.my-slider').slick({
+    dots: true,
+    infinite: false,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
